1e3f102b7ff7a4c08c79f1d54f50f4a06e4408c4
new92 <94779840+new92@users.noreply.github.com>
2023-03-02 14:25:46 +0200

    Update Researcher.py

--- GetLoc/Researcher.py
+++ GetLoc/Researcher.py
@@ -47,7 +47,7 @@ def ScriptInfo():
     language = 'Python'
     name = 'Researcher'
     api = None
-    lines = 276
+    lines = 275
     f = '/Instagram/GetLoc/Researcher.py'
     ptf = os.path.abspath(f)
     fsize = (os.stat(f)).st_size
