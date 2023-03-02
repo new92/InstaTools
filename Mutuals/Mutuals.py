d74389b1846e9119f25a876a3ae302c5b1079e9b
new92 <94779840+new92@users.noreply.github.com>
2023-03-03 00:40:35 +0200

    Update Mutuals.py

--- Mutuals/Mutuals.py
+++ Mutuals/Mutuals.py
@@ -60,7 +60,7 @@ def ScriptInfo():
     forks = 2
     print("[+] Author: "+author)
     print("[+] Github: @"+author)
-    print("[+] License: "+str(license1)
+    print("[+] License: "+license1)
     print("[+] Natural language: "+lang)
     print("[+] Programming language(s) used: "+str(language))
     print("[+] Number of lines: "+str(lines))
