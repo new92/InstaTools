847bf3374e26a5b9fc1f28d2c6557eed5c92db08
new92 <94779840+new92@users.noreply.github.com>
2023-03-22 15:00:24 +0200

    Update Mutuals.py

--- Mutuals/Mutuals.py
+++ Mutuals/Mutuals.py
@@ -63,7 +63,7 @@ def ScriptInfo():
     lang = 'en-US'
     language = 'Python'
     api = None
-    lines = 367
+    lines = 357
     f = '/Instagram/IsVer/IsVer.py'
     if os.path.exists(os.path.abspath(f)):
         fsize = (os.stat(f)).st_size
