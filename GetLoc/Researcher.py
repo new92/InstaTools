12c8d49d6e2017e43350c01e5a19d536226d1523
new92 <94779840+new92@users.noreply.github.com>
2023-04-09 12:36:41 +0300

    Update Researcher.py

--- GetLoc/Researcher.py
+++ GetLoc/Researcher.py
@@ -107,6 +107,7 @@ def ScriptInfo():
     print(f"[+] Number of lines: {lines}")
     print(f"[+] Script's name: {name}")
     print(f"[+] File size: {fsize} bytes")
+    print(f"[+] API used: {api}")
     print(f"[+] Number of stars on the Github repo: {stars}")
     print(f"[+] Number of forks on the Github repo: {forks}")
     print(f"[+] Number of open issues on the Github repo: {issues}")
