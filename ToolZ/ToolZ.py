825f4b29f0070c8b9e2dfa05f21faa2435ff1ff3
new92 <new92github@gmail.com>
2023-10-15 16:14:27 +0300

    Update ToolZ.py

--- ToolZ/ToolZ.py
+++ ToolZ/ToolZ.py
@@ -180,8 +180,8 @@ def ScriptInfo():
     with open('config.json') as config:
         conf = json.load(config)
     f = conf['name'] + '.py'
-    fp = os.path.exists(fpath(f)) if not fpath(f) == None else None
-    fsize = 0 if fp == None else os.stat(fpath(f)).st_size
+    fp = True if not fpath(f) == None else False
+    fsize = 0 if not fp else os.stat(fpath(f)).st_size
     print(f"{YELLOW}[+] Author: {conf['author']}")
     print(f"{YELLOW}[+] Contributors: {conf['contributors']}")
     print(f"{YELLOW}[+] Github: @{conf['author']}")
